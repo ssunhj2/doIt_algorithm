f7026434ab753c9dc2a764dcbdec8a4bece9fdf8
es2sun <wiiro017@gmail.com>
2019-07-23 17:38:43 +0900

    practice01, Exam06 문제풀이

---
+++ src/CH02/exam/Exam06.java
@@ -0,0 +1,47 @@
+package CH02.exam;
+
+/**
+ * 배열을 역순으로 정렬한다.
+ */
+public class Exam06 
+{
+	static void switching(int[] iArr, int index1, int index2)
+	{
+		int temp = iArr[index1];
+		iArr[index1] = iArr[index2];
+		iArr[index2] = temp;
+	}
+	
+	static void reverseArr(int[] iArr)
+	{
+		int arrLength = iArr.length;
+		
+		for(int i=0; i<(arrLength/2); i++)
+		{
+			switching(iArr, i, arrLength-i-1);
+		}
+	}
+	
+	public static void main(String[] args) 
+	{
+		int numCnt = 6;
+		
+		System.out.println("요소수: " + numCnt);
+		
+		int[] numArr = {10, 73, 2, -5, 42, 77};
+		
+		for(int i=0; i<numCnt; i++)
+		{
+			System.out.print("numArr[" + i + "] = " + numArr[i]);
+		}
+		
+		reverseArr(numArr);
+		
+		System.out.print("요소를 역순으로 정렬한다.");
+		for(int i=0; i<numCnt; i++)
+		{
+			System.out.print("numArr[" + i + "] = " + numArr[i]);
+		}
+	}
+
+}
--- src/CH02/practice/Pr01.java
+++ src/CH02/practice/Pr01.java
@@ -3,11 +3,11 @@ package CH02.practice;
 import java.util.Random;
 
 /**
- * 난수를 사용해 배열 요소값 설정한다.
+ * 사람의 수와 몸무게를 난수로 받아 최대값을 구한다.
  **/
 public class Pr01
 {
-    static int maxArr(int[] intArr)
+	static int maxArr(int[] intArr)
     {
         int max = intArr[0];
 
@@ -26,18 +26,16 @@ public class Pr01
     {
         Random random = new Random();
 
-        int personCnt = 5;
+        int personCnt = 1 + random.nextInt(10);
 
         System.out.println("몸무게의 최대값을 구한다.");
-        System.out.print("사람 수 : ");
+        System.out.println("사람 수 : "+ personCnt);
 
-        int num = personCnt;
-
-        int[] weight = new int[num];
+        int[] weight = new int[personCnt];
 
         System.out.println("몸무게는 아래와 같다.");
 
-        for(int i=0; i<num; i++)
+        for(int i=0; i<personCnt; i++)
         {
             weight[i] = 50 + random.nextInt(50);
             System.out.println("weight["+i+"]: " + weight[i]);
