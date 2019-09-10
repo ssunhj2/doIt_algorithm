8b1b34b4ae8fc554ef8ee72e007b9bf1b644b482
es2sun <wiiro017@gmail.com>
2019-09-10 21:51:39 +0900

    CH07 집합 - 연습문제 풀이

---
+++ src/CH07/exam/IntSetTester.java
@@ -0,0 +1,40 @@
+package CH07.exam;
+
+public class IntSetTester {
+
+	public static void main(String[] args) 
+	{
+		IntSet s1 = new IntSet(20);
+		IntSet s2 = new IntSet(20);
+		IntSet s3 = new IntSet(20);
+
+		s1.add(10);			
+		s1.add(15);			
+		s1.add(20);			
+		s1.add(25);			
+
+		s1.copyTo(s2);		
+		s2.add(12);			
+		s2.remove(25);		
+
+		s3.copyFrom(s2);	
+
+		System.out.println("s1 = " + s1);
+		System.out.println("s2 = " + s2);
+		System.out.println("s3 = " + s3);
+
+		System.out.println("집합 s1에 15는 " + (s1.contains(15) ? "포합된다" : "포함되지않는다."));
+
+		System.out.println("집합 s2에 25는 " + (s2.contains(25) ? "포합된다." : "포함되지않는다"));
+
+		System.out.println("집합 s1과 s2는" + (s1.equalTo(s2) ? "같다." : "같지않다."));
+
+		System.out.println("집합 s2와 s3는 " + (s2.equalTo(s3) ? "같다." : "같지않다."));
+
+		s3.unionOf(s1, s2);		// s3에 s1과 s2의 합집합을 복사한다.
+
+		System.out.println("집합 s1과 s2의 합집합은 " + s3 + "이다.");
+
+	}
+
+}
